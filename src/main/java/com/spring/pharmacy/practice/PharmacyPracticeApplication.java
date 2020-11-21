cfbfef09ce9b85d9a28823c2fff08f5865a987eb
jose antonio alvino velasque <jose_antonio_alvino21@outlook.es>
2020-11-21 15:59:19 -0500

    Initial commit

---
+++ src/main/java/com/spring/pharmacy/practice/PharmacyPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.pharmacy.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/pharmacy/practice/PharmacyPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.pharmacy.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
